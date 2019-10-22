d61f94217f078b80e8e47e01f55ae8b3292c2c1b
Eugene Ahn <10797905+PresentLocation@users.noreply.github.com>
2019-10-22 04:02:41 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <!DOCTYPE HTML>
-	<html class="no-js" lang="en" dir="ltr">
-		<head>
-    			<meta charset="utf-8">
-    			<meta http-equiv="x-ua-compatible" content="ie=edge">
-    			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    			<link rel="stylesheet" href="meta/css/foundation.css">
-    			<link rel="stylesheet" href="meta/css/localCustom.css">
-   			<title></title>
-			<meta name="description" content="Description meta tag.">
-			<meta name="keywords" content="Keywords, Go, Here">
-			<meta name="language" content="en">
-			<meta name="robots" content="All">
-		</head>
-		<body>
-			<!-- BEGIN CONTENT LAYOUT HERE -->
+<html class="no-js" lang="en" dir="ltr">
+	<head>
+    		<meta charset="utf-8">
+    		<meta http-equiv="x-ua-compatible" content="ie=edge">
+    		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    		<link rel="stylesheet" href="meta/css/foundation.css">
+    		<link rel="stylesheet" href="meta/css/localCustom.css">
+   		<title></title>
+		<meta name="description" content="Description meta tag.">
+		<meta name="keywords" content="Keywords, Go, Here">
+		<meta name="language" content="en">
+		<meta name="robots" content="All">
+	</head>
+	<body>
+		<!-- BEGIN CONTENT LAYOUT HERE -->
 			
-			<h1>Hello World!</h1>
+		<h1>Hello World!</h1>
 			
-			<!-- END CONTENT LAYOUT HERE -->
+		<!-- END CONTENT LAYOUT HERE -->
 			
-			<script src="meta/js/vendor/jquery.js"></script>
-			<script src="meta/js/vendor/what-input.js"></script>
-    			<script src="meta/js/vendor/foundation.js"></script>
-    			<script src="meta/js/app.js"></script>
-		</body>
+		<script src="meta/js/vendor/jquery.js"></script>
+		<script src="meta/js/vendor/what-input.js"></script>
+    		<script src="meta/js/vendor/foundation.js"></script>
+    		<script src="meta/js/app.js"></script>
+	</body>
 </html>
