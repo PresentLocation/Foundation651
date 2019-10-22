e52db8bdf3ee4bcbabe846a25668c04d0aedfaf3
Eugene Ahn <10797905+PresentLocation@users.noreply.github.com>
2019-10-22 03:34:40 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?
+	
+	
+?>
+<!DOCTYPE HTML>
+<html class="no-js" lang="en" dir="ltr">
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="meta/css/foundation.css">
+    <link rel="stylesheet" href="meta/css/localCustom.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+   <title></title>
+<meta name="description" content="Description meta tag.">
+<meta name="keywords" content="Keywords, Go, Here">
+<meta name="language" content="en">
+<meta name="robots" content="All">
+
+		
+		
+		
+	</head>
+	
+	<body>
+		
+
+
+    <script src="meta/js/vendor/jquery.js"></script>
+    <script src="meta/js/vendor/what-input.js"></script>
+    <script src="meta/js/vendor/foundation.js"></script>
+    <script src="meta/js/app.js"></script>
+
+
+
+	</body>
+	
+	
+</html>
